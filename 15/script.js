42c2567526ad0ea569bb6a74a5e78040914d4da5
Natalia <natalia-321s@outlook.com>
2022-08-31 23:15:17 -0300

    E-commerce (parte 1)

---
+++ 15/script.js
@@ -0,0 +1,11 @@
+feather.replace();
+
+tippy('#promocoes', {
+  content: 'Acesse as promoções da semana',
+});
+tippy('#favoritos', {
+  content: 'Acesse a lista dos seus produtos favoritos',
+});
+tippy('#carrinho', {
+  content: 'Acesse os produtos do carrinho',
+});
